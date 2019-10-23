e78426c1451387562fb12e7114f3583cae226059
Leonardo Setti <leonardosetti@gmail.com>
2019-10-23 16:52:07 -0300

    prototype changes

--- mc.cypress/itf.env/cypress/integration/itf/login/lang/_prototype.pick.lang.spec.js
+++ mc.cypress/itf.env/cypress/integration/itf/login/lang/_prototype.pick.lang.spec.js
@@ -1,21 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('Pick up a Language', function () {
-    it('Land to ITF login page', function () {
+    it('Land to Stage2 login page', function () {
         cy
             .clearCookies()
-            .visit('https://itf.sdg2.mastercard.com/sdportal/home.view?cobrandHost=mastercard')
-            .wait(5000)
-    })
+            .visit('https://stage1.sdg2.mastercard.com')
+
+    });
     it('select French language before login steps', function () {
-        cy.get('select#langSelector').then(function ($select) {
+        cy.
+        get('select#langSelector').then(function ($select) {
                 $select.val('fr').select().click({
                     force: true
                 })
-                //.focus('')
-                ;
 
             })
-            .wait(5000)
+            .focus('#loginUserID')
+            .wait(2000)
     });
 })
