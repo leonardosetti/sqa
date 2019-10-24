d5aeba0423bef30988472597e4ec5d1130733a0d
Leonardo Setti <leonardosetti@gmail.com>
2019-10-24 01:33:17 -0300

    Thu 24 Oct 2019 01:33:17 AM -03 reload and sync from debian

--- mc.cypress/itf.env/cypress/integration/itf/login/lang/load.login.spec.js
+++ mc.cypress/itf.env/cypress/integration/itf/login/lang/load.login.spec.js
@@ -3,39 +3,47 @@
 
 /// <reference types="Cypress" />
 
-describe ('Check Default Cobrand URL Health', function(){
-    it ('Navigate to default URL', function(){
-        cy.visit('https://itf.sdg2.mastercard.com/sdportal/home.view?cobrandHost=mastercard')
+describe('Check Default Cobrand URL Health', function () {
+    it('Navigate to default URL', function () {
+        cy
+        .clearCookie()
+        .clearCookies()
+        .clearLocalStorage()
+        .visit('https://itf.sdg2.mastercard.com/sdportal/home.view?cobrandHost=mastercard')
     })
 })
 
 
 
-describe('Pick a Language', function () {
+/* describe('Pick a Language', function () {
     it('select language', function () {
 cy.get('select#langSelector')
 
 .select('fr').should('have.value','fr')
     })
-})
-
-describe ('Fill User ID Credentials', function(){
-    it ('type user ID in User Id Box', function(){
+}) */
+/* 
+describe('Fill User ID Credentials', function () {
+    it('type user ID in User Id Box', function () {
         cy.get('input#userid.gwt-TextBox').type('e047475')
     })
-    it ('type user password in User Password Box', function(){
+    it('type user password in User Password Box', function () {
         cy.get('input#password.gwt-PasswordTextBox').type('systest01')
-        .wait(5000)
+                })
+}) */
+
+
+describe('Access level 1', function () {
+    it('Submmit credentials to login', function ()  {
+        cy
+            .get().xpath('//*[@id="login-module"]/form/div/div[5]/div/button').click()
+
+
+
     })
 })
 
 
-describe ('Access level 1',function(){
-        it ('Submmit credentials to login',function(){
-            cy.get('button.button.r').click({force:true})
-            .wait(5000)
-        })
-})
 
 /* describe ('Access to Accept Cookies page', function(){
 it ('Access next page Cookies Conscent', function(){
